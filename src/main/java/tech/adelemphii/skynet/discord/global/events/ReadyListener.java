3ce5f020314bf9945738654e07d8d0deb7c18892
Adelemphii <demilitioowner@gmail.com>
2023-02-24 16:53:40 -0600

    properly put yuh4j event in the yuh4j module

--- src/main/java/tech/adelemphii/skynet/discord/global/events/ReadyListener.java
+++ src/main/java/tech/adelemphii/skynet/discord/global/events/ReadyListener.java
@@ -1,30 +1,18 @@
 package tech.adelemphii.skynet.discord.global.events;
 
-import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
-import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 import tech.adelemphii.skynet.discord.DiscordBot;
-import tech.adelemphii.skynet.discord.global.objects.Server;
-import tech.adelemphii.skynet.discord.yuh4j.objects.Mission;
-import tech.adelemphii.skynet.discord.yuh4j.utility.Yuh4jMessageUtility;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 public class ReadyListener implements EventListener {
 
-    private final List<Long> remindersSent;
 
     private final DiscordBot discordBot;
 
     public ReadyListener(DiscordBot discordBot) {
         this.discordBot = discordBot;
-        this.remindersSent = new ArrayList<>();
     }
 
     @Override
@@ -33,66 +21,18 @@ public class ReadyListener implements EventListener {
             discordBot.getPlugin().getLogger().info("API is ready!");
         }
 
+        /*
         if(event instanceof GuildReadyEvent) {
             GuildReadyEvent guildReadyEvent = (GuildReadyEvent) event;
 
+            /*
             Server server = discordBot.getServerConfiguration().getServer(guildReadyEvent.getGuild().getIdLong());
-
-            if(server != null) {
-                Yuh4jMessageUtility.sendMessage(guildReadyEvent.getGuild(), discordBot);
-            }
-
-            BukkitRunnable yuh4jRunnable = new BukkitRunnable() {
-                @Override
-                public void run() {
-                    if(server == null || !server.getYuh4jServer().isEnabled()) {
-                        return;
-                    }
-
-                    int timeBeforeOp = server.getYuh4jServer().getMinutesBeforeOpTimer();
-
-                    ArrayList<Mission> missions = Yuh4jMessageUtility.scrapeMessages(guildReadyEvent.getGuild(), server);
-                    if(missions == null) {
-                        return;
-                    }
-
-                    for (Mission mission : missions) {
-                        if(remindersSent.contains(mission.getMessage().getIdLong())) {
-                            continue;
-                        }
-
-                        // -------------------------------------------------------------------- X minutes in UNIX timestamp
-                        Map<String, MessageEmbed> messageEmbedMap = Yuh4jMessageUtility.getPingsWhenTime(mission,
-                                timeBeforeOp * 60000L / 1000L);
-                        if(messageEmbedMap == null || messageEmbedMap.isEmpty()) {
-                            return;
-                        }
-
-                        String text = messageEmbedMap.keySet().stream().findFirst().get();
-                        MessageEmbed messageEmbed = messageEmbedMap.values().stream().findFirst().get();
-
-                        String error = Yuh4jMessageUtility.sendBreadPings(guildReadyEvent.getGuild(), discordBot, messageEmbed,
-                                missions, text, timeBeforeOp);
-                        if(error != null) {
-                            discordBot.getPlugin().getLogger().severe("Error: " + error);
-                            return;
-                        }
-
-                        remindersSent.add(mission.getMessage().getIdLong());
-                    }
-                }
-            };
-
-            // 10 seconds
-            int yuh4jLength = 10 * 20;
-            yuh4jRunnable.runTaskTimerAsynchronously(discordBot.getPlugin(), 0, yuh4jLength);
-            discordBot.addUpdateRunnable(server, yuh4jRunnable);
 
             BukkitRunnable forumScraperRunnable = new BukkitRunnable() {
                 @Override
                 public void run() {
-                    /* TODO: Errors when this runs, disable for now; it also doesn't save the message when
-                            running the command, sometimes?.
+                    // TODO: Errors when this runs, disable for now; it also doesn't save the message when
+                             running the command, sometimes?.
                     if(server == null || !server.getForumScraperServer().isEnabled()) {
                         return;
                     }
@@ -100,7 +40,6 @@ public class ReadyListener implements EventListener {
                     ScrapeUtility.sendPopularTopics(guildReadyEvent.getGuild());
                     ScrapeUtility.sendLatestTopics(guildReadyEvent.getGuild());
                     ScrapeUtility.sendPingUpdate(guildReadyEvent.getGuild());
-                    */
                 }
             };
 
@@ -109,5 +48,6 @@ public class ReadyListener implements EventListener {
             forumScraperRunnable.runTaskTimerAsynchronously(discordBot.getPlugin(), 0, forumScraperLength);
             discordBot.addUpdateRunnable(server, forumScraperRunnable);
         }
+        */
     }
 }
--- src/main/java/tech/adelemphii/skynet/discord/yuh4j/events/ScheduleListener.java
+++ src/main/java/tech/adelemphii/skynet/discord/yuh4j/events/ScheduleListener.java
@@ -2,35 +2,44 @@ package tech.adelemphii.skynet.discord.yuh4j.events;
 
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.channel.ChannelType;
 import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageDeleteEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 import tech.adelemphii.skynet.discord.DiscordBot;
+import tech.adelemphii.skynet.discord.yuh4j.objects.Mission;
 import tech.adelemphii.skynet.discord.yuh4j.utility.Yuh4jMessageUtility;
 import tech.adelemphii.skynet.discord.global.objects.Server;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class ScheduleListener implements EventListener {
 
     private final DiscordBot discordBot;
+    private final List<Long> remindersSent = new ArrayList<>();
 
     public ScheduleListener(DiscordBot discordBot) {
         this.discordBot = discordBot;
     }
 
     @Override
-    public void onEvent(@NotNull GenericEvent genericEvent) {
-        if(genericEvent instanceof MessageReceivedEvent) {
-            MessageReceivedEvent event = (MessageReceivedEvent) genericEvent;
-            Guild guild = event.getGuild();
+    public void onEvent(@NotNull GenericEvent event) {
+        if(event instanceof MessageReceivedEvent) {
+            MessageReceivedEvent messageReceivedEvent = (MessageReceivedEvent) event;
+            Guild guild = messageReceivedEvent.getGuild();
             Server server = discordBot.getServerConfiguration().getServer(guild.getIdLong());
-            Message message = event.getMessage();
+            Message message = messageReceivedEvent.getMessage();
 
-            if(event.getChannelType() == ChannelType.TEXT) {
-                if(event.getChannel().asTextChannel().getIdLong() == server.getYuh4jServer().getScheduleChannel()) {
+            if(messageReceivedEvent.getChannelType() == ChannelType.TEXT) {
+                if(messageReceivedEvent.getChannel().asTextChannel().getIdLong() == server.getYuh4jServer().getScheduleChannel()) {
                     String error = Yuh4jMessageUtility.sendMessage(guild, discordBot);
                     if(error != null) {
                         message.reply("Error: " + error).queue();
@@ -39,14 +48,14 @@ public class ScheduleListener implements EventListener {
             }
         }
 
-        if(genericEvent instanceof MessageUpdateEvent) {
-            MessageUpdateEvent event = (MessageUpdateEvent) genericEvent;
-            Guild guild = event.getGuild();
+        if(event instanceof MessageUpdateEvent) {
+            MessageUpdateEvent messageUpdateEvent = (MessageUpdateEvent) event;
+            Guild guild = messageUpdateEvent.getGuild();
             Server server = discordBot.getServerConfiguration().getServer(guild.getIdLong());
-            Message message = event.getMessage();
+            Message message = messageUpdateEvent.getMessage();
 
-            if(event.getChannelType() == ChannelType.TEXT) {
-                if(event.getChannel().asTextChannel().getIdLong() == server.getYuh4jServer().getScheduleChannel()) {
+            if(messageUpdateEvent.getChannelType() == ChannelType.TEXT) {
+                if(messageUpdateEvent.getChannel().asTextChannel().getIdLong() == server.getYuh4jServer().getScheduleChannel()) {
                     String error = Yuh4jMessageUtility.sendMessage(guild, discordBot);
                     if(error != null) {
                         message.reply("Error: " + error).queue();
@@ -54,20 +63,76 @@ public class ScheduleListener implements EventListener {
                 }
             }
         }
-        if(genericEvent instanceof MessageDeleteEvent) {
-            MessageDeleteEvent event = (MessageDeleteEvent) genericEvent;
+        if(event instanceof MessageDeleteEvent) {
+            MessageDeleteEvent messageDeleteEvent = (MessageDeleteEvent) event;
 
-            Guild guild = event.getGuild();
+            Guild guild = messageDeleteEvent.getGuild();
             Server server = discordBot.getServerConfiguration().getServer(guild.getIdLong());
 
-            if(event.getChannelType() == ChannelType.TEXT) {
-                if(event.getChannel().asTextChannel().getIdLong() == server.getYuh4jServer().getScheduleChannel()) {
+            if(messageDeleteEvent.getChannelType() == ChannelType.TEXT) {
+                if(messageDeleteEvent.getChannel().asTextChannel().getIdLong() == server.getYuh4jServer().getScheduleChannel()) {
                     String error = Yuh4jMessageUtility.sendMessage(guild, discordBot);
                     if(error != null) {
                         discordBot.getPlugin().getLogger().severe("[DiscordBot] Error:" + error);
                     }
                 }
             }
+        }
+
+        if(event instanceof GuildReadyEvent) {
+            GuildReadyEvent guildReadyEvent = (GuildReadyEvent) event;
+
+            Server server = discordBot.getServerConfiguration().getServer(guildReadyEvent.getGuild().getIdLong());
+
+            if(server != null) {
+                Yuh4jMessageUtility.sendMessage(guildReadyEvent.getGuild(), discordBot);
+            }
+
+            BukkitRunnable yuh4jRunnable = new BukkitRunnable() {
+                @Override
+                public void run() {
+                    if(server == null || !server.getYuh4jServer().isEnabled()) {
+                        return;
+                    }
+
+                    int timeBeforeOp = server.getYuh4jServer().getMinutesBeforeOpTimer();
+
+                    ArrayList<Mission> missions = Yuh4jMessageUtility.scrapeMessages(guildReadyEvent.getGuild(), server);
+                    if(missions == null) {
+                        return;
+                    }
+
+                    for (Mission mission : missions) {
+                        if(remindersSent.contains(mission.getMessage().getIdLong())) {
+                            continue;
+                        }
+
+                        // -------------------------------------------------------------------- X minutes in UNIX timestamp
+                        Map<String, MessageEmbed> messageEmbedMap = Yuh4jMessageUtility.getPingsWhenTime(mission,
+                                timeBeforeOp * 60000L / 1000L);
+                        if(messageEmbedMap == null || messageEmbedMap.isEmpty()) {
+                            return;
+                        }
+
+                        String text = messageEmbedMap.keySet().stream().findFirst().get();
+                        MessageEmbed messageEmbed = messageEmbedMap.values().stream().findFirst().get();
+
+                        String error = Yuh4jMessageUtility.sendBreadPings(guildReadyEvent.getGuild(), discordBot, messageEmbed,
+                                missions, text, timeBeforeOp);
+                        if(error != null) {
+                            discordBot.getPlugin().getLogger().severe("Error: " + error);
+                            return;
+                        }
+
+                        remindersSent.add(mission.getMessage().getIdLong());
+                    }
+                }
+            };
+
+            // 10 seconds
+            int yuh4jLength = 10 * 20;
+            yuh4jRunnable.runTaskTimerAsynchronously(discordBot.getPlugin(), 0, yuh4jLength);
+            discordBot.addUpdateRunnable(server, yuh4jRunnable);
         }
     }
 }
